94a5eefd1507ee16cbdcc7404524507a08ffcf8f
vobile_lzl <824337531@qq.com>
2017-05-22 19:28:18 +0800

    #Comment 创建book_recommend工程

---
+++ src/main/java/SpiderBooks.java
@@ -0,0 +1,10 @@
+/**
+ * @Author: li_zhilei
+ * @Date: create in 19:25 17/5/22.
+ * @description:
+ */
+public class SpiderBooks {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
