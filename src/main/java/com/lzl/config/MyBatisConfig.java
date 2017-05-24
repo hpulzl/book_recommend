debfbb6b07d83a2b1f5f269ca6691b5654d3c5a8
vobile_lzl <824337531@qq.com>
2017-05-24 19:47:42 +0800

    #Comment 环境搭建

--- src/main/java/com/lzl/config/MyBatisConfig.java
+++ src/main/java/com/lzl/config/MyBatisConfig.java
@@ -2,16 +2,26 @@ package com.lzl.config;
 
 import javax.sql.DataSource;
 
+import com.github.pagehelper.PageHelper;
+import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
 
 /**
  * @author zxj
@@ -22,19 +32,43 @@ import org.springframework.transaction.annotation.TransactionManagementConfigure
 @EnableTransactionManagement
 public class MyBatisConfig implements TransactionManagementConfigurer {
     @Autowired
-    private DataSource dataSource;
-
-    @Override
-    public PlatformTransactionManager annotationDrivenTransactionManager() {
-         return new DataSourceTransactionManager(dataSource);
-    }
+    DataSource dataSource;
 
     @Bean(name = "sqlSessionFactory")
     public SqlSessionFactory sqlSessionFactoryBean() {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
+        bean.setTypeAliasesPackage("cn.vobile.hss.model");
 
+        // 支持下划线到驼峰
+        org.apache.ibatis.session.Configuration conf = new org.apache.ibatis.session.Configuration();
+        conf.setMapUnderscoreToCamelCase(true);
+        bean.setConfiguration(conf);
+
+        //分页插件
+        PageHelper pageHelper = new PageHelper();
+        Properties properties = new Properties();
+        properties.setProperty("offsetAsPageNum", "true");
+        properties.setProperty("rowBoundsWithCount", "true");
+        properties.setProperty("reasonable", "true");
+        properties.setProperty("supportMethodsArguments", "true");
+        properties.setProperty("returnPageInfo", "check");
+        properties.setProperty("params", "count=countSql");
+        pageHelper.setProperties(properties);
+
+        //添加插件
+        bean.setPlugins(new Interceptor[]{pageHelper});
+
+        //添加XML目录
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         try {
+            //读取多个文件下的xml文件
+            List<Resource> resources = new ArrayList<>(8);
+            Resource[] resources1 = resolver.getResources("classpath*:mapper/*.xml");
+            Resource[] resources2 = resolver.getResources("classpath*:sqlmapperext/*.xml");
+            resources.addAll(Arrays.asList(resources1));
+            resources.addAll(Arrays.asList(resources2));
+            bean.setMapperLocations(resources.toArray(new Resource[]{}));
             return bean.getObject();
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,5 +79,11 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
     @Bean
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
         return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
